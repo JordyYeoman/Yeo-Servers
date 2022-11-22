359e455c5eeda7244182f41a3921542419e68c5b
Jordy Yeoman <jordy.yeoman@entaingroup.com.au>
2022-11-22 14:59:00 +0800

    Creating generic express backend template

---
+++ express-template/src/api/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import todos from './todos/todos.routes';
+import MessageResponse from '../interfaces/MessageResponse';
+import emojis from './emojis';
+
+const router = express.Router();
+
+router.get<{}, MessageResponse>('/', (req, res) => {
+  res.json({
+    message: 'API - 👋🌎🌍🌏',
+  });
+});
+
+router.use('/emojis', emojis);
+router.use('/todos', todos);
+
+export default router;
